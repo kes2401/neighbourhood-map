d5fc8dce80d758d1dda961b3c8bb5bd85bf0769c
kes2401 <keithscully83@gmail.com>
2018-07-30 23:17:50 +0100

    add Locations list and Map components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Locations from './Locations';
+import MapComponent from './MapComponent';
 
 
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="main-header">Neighbourhood Map</h1>
+          <div className="hamburger-icon">
+            <div className="hamburger-menu-button"></div>
+            <div className="hamburger-menu-button"></div>
+            <div className="hamburger-menu-button"></div>
+          </div>
+          <h1 className="main-header-text">Neighbourhood Map</h1>
         </header>
+        <Locations />
+        <MapComponent />
       </div>
     );
   }
