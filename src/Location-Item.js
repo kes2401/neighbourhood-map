10f475102b40816ef784a835ed4c79029c18b09e
kes2401 <keithscully83@gmail.com>
2018-08-01 23:23:46 +0100

    add location-item component and add locations to location list pane

---
+++ src/Location-Item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const locationItem = (props) => {
+
+	return (
+
+		<li className="list-item">
+			<p className="list-item-text">{props.locationName}</p>">
+		</li>
+	)
+}
+
+export default locationItem;
